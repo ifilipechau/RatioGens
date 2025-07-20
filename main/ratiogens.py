46581524278ccfef99c057dbb2fdca3680ba03aa
ifilipechau <filipechau.dev@gmail.com>
2025-07-20 18:04:39 +0200

    add: implementar a funcao inserir equipamento para adicionar equipamentos

--- main/ratiogens.py
+++ main/ratiogens.py
@@ -3,3 +3,15 @@
 
 # Importa bibliotecas necessárias
 import datetime
+
+def inserir_equipamento():
+    equipamentos = []
+    n = int(input("Quantos equipamentos deseja inserir? "))
+
+    for i in range(n):
+        print(f"\nEquipamento {i+1}:")
+        nome = input("Nome: ")
+        tipo = input("Tipo (iluminacao/motor/equipamento comum/sensível): ").lower()
+        potencia = float(input("Potência (W): "))
+        quantidade = int(input("Quantidade: "))
+        tempo_uso = float(input("Horas de uso por dia (opcional - 0 se desconhecido): "))
