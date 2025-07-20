d49aa140dc3b7239bc82a94f09cbca2c4ae15de5
ifilipechau <filipechau.dev@gmail.com>
2025-07-20 18:11:05 +0200

    add: implementar a funcao exportar relatorio

--- main/ratiogens.py
+++ main/ratiogens.py
@@ -35,3 +35,18 @@ def calcular_potencia_total(equipamentos):
     for e in equipamentos:
         total += e['potencia'] * e['quantidade']
     return total
+
+def exportar_relatorio(equipamentos, potencia_total, potencia_kva, factor_potencia, factor_seguranca, energia_total_diaria):
+    with open("relatorio_gerador.txt", "w") as f:
+        f.write("RELATÓRIO DE DIMENSIONAMENTODE GERADOR\n")
+        f.write(f"Data: {datetime.datetime.now()}\n\n")
+
+        for eq in equipamentos:
+            f.write(f"- {eq['quantidade']}x {eq['nome']} ({eq['potencia']}W) - Tipo: {eq['tipo']}\n")
+        f.write(f"\nPotência total: {potencia_total: .2f} W\n")
+        f.write(f"Factor de potência: {factor_potencia}\n")
+        f.write(f"Factor de segurança: {factor_seguranca}\n")
+        f.write(f"Potência ideal do gerador: {round(potencia_kva, 2)} kVA\n")
+        if energia_total_diaria > 0:
+            f.write(f"Consumo estimado diário: {round(energia_total_diaria/1000, 2)} Kwh\n")
+        print("Relatório exportado com sucesso: relatorio_gerador.txt")
